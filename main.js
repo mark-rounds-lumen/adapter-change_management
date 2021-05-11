ce70067d25f1683405627d981f8e9a569823ce4d
mark-rounds-lumen <mark.rounds@lumen.com>
2021-05-11 00:45:23 +0000

    Lab 6

--- main.js
+++ main.js
@@ -83,6 +83,26 @@ class ServiceNowAdapter extends EventEmitter {
     this.healthcheck();
   }
 
+  /**
+   * @memberof ServiceNowAdapter
+   * @method healthcheck
+   * @summary Check ServiceNow Health
+   * @description Verifies external system is available and healthy.
+   *   Calls method emitOnline if external system is available.
+   *
+   * @param {ServiceNowAdapter~requestCallback} [callback] - The optional callback
+   *   that handles the response.
+   */
+  /**
+ * @memberof ServiceNowAdapter
+ * @method healthcheck
+ * @summary Check ServiceNow Health
+ * @description Verifies external system is available and healthy.
+ *   Calls method emitOnline if external system is available.
+ *
+ * @param {ServiceNowAdapter~requestCallback} [callback] - The optional callback
+ *   that handles the response.
+ */
 /**
  * @memberof ServiceNowAdapter
  * @method healthcheck
@@ -93,7 +113,8 @@ class ServiceNowAdapter extends EventEmitter {
  * @param {ServiceNowAdapter~requestCallback} [callback] - The optional callback
  *   that handles the response.
  */
-healthcheck(callback) {
+ healthcheck(callback) {
+    
  this.getRecord((result, error) => {
    /**
     * For this lab, complete the if else conditional
@@ -101,6 +122,7 @@ healthcheck(callback) {
     * or the instance was hibernating. You must write
     * the blocks for each branch.
     */
+    
    if (error) {
      /**
       * Write this block.
@@ -114,8 +136,16 @@ healthcheck(callback) {
       * healthcheck(), execute it passing the error seen as an argument
       * for the callback's errorMessage parameter.
       */
-      this.emitOffline();
-   } else {
+      log.error(`External system ${this.id} is temporarily down for maintenance`);
+
+      this.emitOffline()
+      if(callback){
+          callback(error)
+      }
+   } 
+
+
+   else {
      /**
       * Write this block.
       * If no runtime problems were detected, emit ONLINE.
@@ -126,9 +156,66 @@ healthcheck(callback) {
       * parameter as an argument for the callback function's
       * responseData parameter.
       */
-      this.emitOnline();
+      //console.log(result)
+      log.debug("Calling ServiceNowAdaptor system's method healthcheck().")
+      console.log("I am inside else block ...................")
+      this.emitOnline()
+      if(callback){
+          callback(result)
+      }
    }
  });
+
+
+ this.postRecord((result, error) => {
+   /**
+    * For this lab, complete the if else conditional
+    * statements that check if an error exists
+    * or the instance was hibernating. You must write
+    * the blocks for each branch.
+    */
+    
+   if (error) {
+     /**
+      * Write this block.
+      * If an error was returned, we need to emit OFFLINE.
+      * Log the returned error using IAP's global log object
+      * at an error severity. In the log message, record
+      * this.id so an administrator will know which ServiceNow
+      * adapter instance wrote the log message in case more
+      * than one instance is configured.
+      * If an optional IAP callback function was passed to
+      * healthcheck(), execute it passing the error seen as an argument
+      * for the callback's errorMessage parameter.
+      */
+      log.error(`External system ${this.id} is temporarily down for maintenance`);
+      this.emitOffline()
+       
+      if(callback){
+          callback(error)
+      }
+   } 
+
+   else {
+     /**
+      * Write this block.
+      * If no runtime problems were detected, emit ONLINE.
+      * Log an appropriate message using IAP's global log object
+      * at a debug severity.
+      * If an optional IAP callback function was passed to
+      * healthcheck(), execute it passing this function's result
+      * parameter as an argument for the callback function's
+      * responseData parameter.
+      */
+      //console.log(result)
+      log.debug("Calling ServiceNowAdaptor system's method healthcheck().")
+      this.emitOnline()
+      if(callback){
+          callback(result)
+      }
+   }
+ })
+ 
 }
 
   /**
@@ -178,16 +265,53 @@ healthcheck(callback) {
    *   handles the response.
    */
   getRecord(callback) {
+
     /**
      * Write the body for this function.
      * The function is a wrapper for this.connector's get() method.
      * Note how the object was instantiated in the constructor().
      * get() takes a callback function.
      */
-    let getCallOptions = { ...this.options };
-    getCallOptions.method = 'GET';
-    getCallOptions.query = 'sysparm_limit=1';
-    this.sendRequest(getCallOptions, (results, error) => callback(results, error));
+     this.connector.get( (data, error) => {
+     var changeTicket={}
+     var ArrayObj = []
+     
+ for(var key in data)
+{
+    if(key === "body")
+{
+    var obj= data[key];
+    var obj2=JSON.parse(obj);
+    var obj3=obj2.result;
+
+    
+
+    console.log("the value inside post.................",obj3)
+for(let i=0; i<obj3.length;i++)
+{
+    changeTicket ={
+      "change_ticket_number":obj3[i].number,
+      "active":obj3[i].active,
+      "priority":obj3[i].priority,
+      "description":obj3[i].description,
+      "work_start":obj3[i].work_start,
+      "work_end":obj3[i].work_end,
+      "change_ticket_key":obj3[i].sys_id
+  }
+
+  ArrayObj.push(changeTicket)
+}
+   
+}} 
+
+console.log("the value is:.....",ArrayObj)
+
+data=ArrayObj
+
+//data["body"]["result"]= returnVar
+callback(data,error)
+
+     });
   }
 
   /**
@@ -199,16 +323,49 @@ healthcheck(callback) {
    * @param {ServiceNowAdapter~requestCallback} callback - The callback that
    *   handles the response.
    */
-  postRecord(callback) {
+ postRecord(callback) {
+
     /**
      * Write the body for this function.
      * The function is a wrapper for this.connector's post() method.
      * Note how the object was instantiated in the constructor().
      * post() takes a callback function.
      */
-    let getCallOptions = { ...this.options };
-    getCallOptions.method = 'POST';
-    this.sendRequest(getCallOptions, (results, error) => callback(results, error));
+     this.connector.post( (data, error) => {
+     var newChangeTicket={}
+     
+     
+ for(var key in data)
+{
+    if(key === "body")
+{
+    var obj= data[key];
+    var obj2=JSON.parse(obj);
+    var obj3=obj2.result;
+
+    
+
+    console.log("the value inside post.................",obj3)
+
+    newChangeTicket ={
+      "change_ticket_number":obj3.number,
+      "active":obj3.active,
+      "priority":obj3.priority,
+      "description":obj3.description,
+      "work_start":obj3.work_start,
+      "work_end":obj3.work_end,
+      "change_ticket_key":obj3.sys_id
+  }
+}} 
+
+
+data=newChangeTicket
+
+//data["body"]["result"]= returnVar
+callback(data,error)
+
+     });
+
   }
 }
 
